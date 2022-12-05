a70e40b4b15daf55808aea1503f85e9db38354c7
Demirkol, Goerkem <gdemirkol315@gmail.com>
2022-12-05 09:02:55 +0100

    bootstrap implemented objects saved into repositories

--- src/main/java/tech/demirkol/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/tech/demirkol/spring5webapp/bootstrap/BootStrapData.java
@@ -2,6 +2,8 @@ package tech.demirkol.spring5webapp.bootstrap;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import tech.demirkol.spring5webapp.domain.Author;
+import tech.demirkol.spring5webapp.domain.Book;
 import tech.demirkol.spring5webapp.repositories.AuthorRepository;
 import tech.demirkol.spring5webapp.repositories.BookRepository;
 
@@ -18,6 +20,24 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evens");
+        Book book1 = new Book("Domain Driven Dev","1231312");
+        eric.getBooks().add(book1);
+        book1.getAuthors().add(eric);
 
+        Author osman = new Author("Osman", "Abi");
+        Book book2 = new Book("Osman Driven Dev","121231312");
+        osman.getBooks().add(book2);
+        book1.getAuthors().add(osman);
+
+        authorRepository.save(eric);
+        authorRepository.save(osman);
+
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+
+        System.out.println("Bootstrap starting!");
+        System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Number of author: " + authorRepository.count());
     }
 }
